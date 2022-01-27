8c6cba6e3272401172151922caa625aa89d6df98
ADM <ADM@A320M-H-CF>
2022-01-28 02:16:24 +0300

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ребро куба:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int S, V;
+            FindCub(x, out S, out V);
+            Console.WriteLine("S={0:f2}м2,V={1:f2}м3", S, V);
+        }
+        static void FindCub(int x, out int S, out int V)
+        {
+            V = x * x * x;
+            S = 6 * x * x;
+        }
+    }
+}
---
+++ 7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Три стороны первого треугольника:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+            double S1 = FindS(x, y, z);
+            Console.WriteLine("S1={0:f2}", S1);
+            Console.WriteLine("Три стороны второго треугольника:");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int z1 = Convert.ToInt32(Console.ReadLine());
+            double S2 = FindS(x1, y1, z1);
+            Console.WriteLine("S2={0:f2}", S2);
+            if (S1 > S2)
+            {
+                Console.WriteLine("Площадь первого треугольника больше");
+            }
+            if (S1 < S2)
+            {
+                Console.WriteLine("Площадь второго треугольника больше");
+            }
+            else
+                Console.WriteLine("Площади равны");
+        }
+        static double FindS(int x, int y, int z)
+        {
+
+            double p = (x + y + z) / 2;
+            double d =
+            p * (p - x) * (p - y) * (p - z);
+            double S = Math.Sqrt(d);
+            return S;
+        }
+    }
+}
